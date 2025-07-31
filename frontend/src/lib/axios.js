da55847ddcf86563cf9771af4e7b0660493c09ef
masroor7086 <masroorahmed4443@gmail.com>
2025-07-31 22:13:24 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+const isDev = import.meta.env.MODE === "development";
+
+const baseURL = isDev
+  ? "http://localhost:5001" // when running locally
+  : "/api"; // proxy to backend-service via Ingress or NGINX in Kubernetes
+
 export const axiosInstance = axios.create({
-  baseURL: "/api", // ✅ Let nginx proxy /api → backend
+  baseURL,
   withCredentials: true,
 });
