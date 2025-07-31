e206a5f4b75ae9b0851688af5b43b033c9a92fdc
masroor7086 <masroorahmed4443@gmail.com>
2025-07-31 22:15:16 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5001"
-  : "http://backend-service:5001"; // ✅ Kubernetes service name
+  : window.location.origin; // will use current HTTPS host
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
