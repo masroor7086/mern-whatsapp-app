05ca26fb38610c323d34d559ba18d26cf71adff5
masroor7086 <masroorahmed4443@gmail.com>
2025-08-02 01:55:06 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,27 @@
-import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
+import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 
-import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
-import { useEffect } from "react";
-
-import { Loader } from "lucide-react";
-import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
   const { theme } = useThemeStore();
 
-  console.log({ onlineUsers });
-
   useEffect(() => {
-    checkAuth();
+    checkAuth(); // ✅ Auto-check auth token on app load
   }, [checkAuth]);
 
-  console.log({ authUser });
-
-  if (isCheckingAuth && !authUser)
+  if (isCheckingAuth)
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
@@ -49,4 +44,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
